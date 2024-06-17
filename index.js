aadf35020e84fef920208bc443d72e5b2915032d
David M <62346025+aboutdavid@users.noreply.github.com>
2024-06-17 09:59:30 -0400

    !boolean

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const getUrls = require('get-urls');
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: Boolean(process.env.PORT),
+    socketMode: !Boolean(process.env.PORT),
     appToken: process.env.SLACK_APP_TOKEN,
     port: process.env.PORT
 });
